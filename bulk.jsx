8e439e2e2bd1c539750ff79436c90abb518043eb
Taro Hanamura <m@hanamurataro.com>
2013-12-19 18:05:05 +0900

    Add Mode

--- bulk.jsx
+++ bulk.jsx
@@ -79,6 +79,23 @@
 }).call(this);
 
 (function() {
+	// constructor
+	var Mode = function(options) {
+		this.options = _.extend({
+			mode: ChangeMode.RGB
+		}, options);
+	};
+	// methods
+	Mode.prototype = {
+		proceed: function(options) {
+			options.doc.changeMode(this.options.mode);
+		}
+	};
+	this.bulk || this.bulk = {};
+	this.bulk.Mode = Mode;
+}).call(this);
+
+(function() {
 	var Options = {};
 	// jpg
 	Options.jpg = function(options) {
