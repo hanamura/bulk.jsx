5d3d1ef502595cc28317f45e9dd237e3b74a0fa8
Taro Hanamura <m@hanamurataro.com>
2013-12-26 18:03:31 +0900

    Add Crop

--- bulk_.jsx
+++ bulk_.jsx
@@ -1400,6 +1400,27 @@
 
 (function() {
 	// constructor
+	var Crop = function(options) {
+		this.options = _.extend({
+			width: 0,
+			height: 0,
+			anchor: AnchorPosition.MIDDLECENTER
+		}, options);
+	};
+	// methods
+	Crop.prototype = {
+		proceed: function(options) {
+			var width = Math.min(this.options.width, options.doc.width);
+			var height = Math.min(this.options.height, options.doc.height);
+			options.doc.resizeCanvas(width, height, this.options.anchor);
+		}
+	};
+	this.bulk || this.bulk = {};
+	this.bulk.Crop = Crop;
+}).call(this);
+
+(function() {
+	// constructor
 	var Mode = function(options) {
 		this.options = _.extend({
 			mode: ChangeMode.RGB
